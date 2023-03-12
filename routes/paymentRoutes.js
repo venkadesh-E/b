b262d1587846aff2de1791ea98a5ba41068fb5a1
venkadesh-E <108859851+venkadesh-E@users.noreply.github.com>
2023-03-12 11:00:55 +0530

    Add files via upload

---
+++ routes/paymentRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {  checkout,paymentVerification } from "../controllers/paymentController.js";
+
+const app=express()
+const router = express.Router();
+router.route("/checkout").post(checkout)
+router.route("/paymentverification").post(paymentVerification)
+
+export default router
